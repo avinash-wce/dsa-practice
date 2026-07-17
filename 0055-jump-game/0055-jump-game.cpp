758451abb0e7ba83216dec67b54d80c96c09ea17
avinash_jadhao <avinashjadhao2007@gmail.com>
2026-07-17 16:22:18 +0530

    Time: 4 ms (22.44%), Space: 52.4 MB (58.62%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int max_reachable = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (i > max_reachable) {
+                return false;
+            }
+            max_reachable = max(max_reachable, i + nums[i]);
+        }
+        
+        return max_reachable >= n - 1;
+    }
+};
