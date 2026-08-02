c7858895405fd3be9c64fee45a886ac9bcbe96b5
avinash_jadhao <avinashjadhao2007@gmail.com>
2026-08-02 12:18:47 +0530

    Time: 0 ms (100%), Space: 26 MB (57.63%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,3 +1,4 @@
+/* TC = O(n), S
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -15,6 +16,30 @@ public:
         }
         for(int i = 0; i < n; i++){
             ans+= min(lmax[i], rmax[i]) - height[i];
+        }
+        return ans;
+    }
+};
+*/
+
+//Two pointer TC : O(n), SC : O(1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0, r = n-1;
+        int lmax = 0, rmax = 0;
+        int ans = 0;
+        while(l < r){
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
+            if (lmax < rmax){
+                ans += lmax - height[l];
+                l++;
+            }else{
+                ans += rmax - height[r];
+                r--;
+            }
         }
         return ans;
     }
