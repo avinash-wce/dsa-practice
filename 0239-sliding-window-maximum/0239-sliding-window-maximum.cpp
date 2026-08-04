b7034cc87d3f8bf0fc056cb696ecfaf0d95909a5
avinash_jadhao <avinashjadhao2007@gmail.com>
2026-08-04 19:22:29 +0530

    Time: 18 ms (77.68%), Space: 139.1 MB (71.19%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -14,7 +14,7 @@ public:
     }
 }; */
 
-class Solution {
+/*class Solution { //tutorial + my solution
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
@@ -41,6 +41,38 @@ public:
             dq.push_back(i);
         }
         ans.push_back(nums[dq.front()]);
+        return ans;
+    }
+}; */
+
+//cleaner AI Imlementation in one loop though TC and SC are identical
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+        deque<int> dq;
+
+        for (int i = 0; i < n; i++) {
+            // 1. Remove indices outside the current window range [i - k + 1, i]
+            if (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            // 2. Remove indices with values smaller than the current element
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            // 3. Add current element's index
+            dq.push_back(i);
+
+            // 4. Record maximum once the first window of size k is formed
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+
         return ans;
     }
 };
