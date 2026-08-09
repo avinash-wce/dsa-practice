89d31931f4d11e9d8d168f40f81325e2fa9118bd
avinash_jadhao <avinashjadhao2007@gmail.com>
2026-08-09 22:53:28 +0530

    Time: 191 ms (15.43%), Space: 226.1 MB (12.32%) - LeetHub

--- 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.cpp
+++ 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     double minPrice(vector<int>& prices, vector<int>& discounts) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         sort(prices.rbegin(), prices.rend());
         sort(discounts.rbegin(), discounts.rend());
         int p = prices.size(), d = discounts.size();
         int i = 0;
         double ans = 0;
         while(i < p && i < d){
-            ans += (prices[i] * (100 - discounts[i])) / 100.0;
+            ans += (prices[i] * (100.0 - discounts[i])) / 100.0;
             i++;
         }
         while(i < p){
